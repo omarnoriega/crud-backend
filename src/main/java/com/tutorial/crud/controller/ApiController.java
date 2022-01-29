3f262da61662afa35d1fc4637ea10437ba55a523
Omar Alfredo Noriega Niebles - Ceiba Software <omar.noriega@ceiba.com.co>
2022-01-28 23:51:52 -0500

    actualización api controller

---
+++ src/main/java/com/tutorial/crud/controller/ApiController.java
@@ -0,0 +1,24 @@
+package com.tutorial.crud.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController()
+@RequestMapping("/")
+public class ApiController {
+    @GetMapping("")
+    public ResponseEntity ping(){
+        Map<String,String> map=new HashMap<String,String>();
+        map.put("USERNAME",System.getenv("USERNAME"));
+        map.put("USERDNSDOMAIN",System.getenv("USERDNSDOMAIN"));
+        map.put("PROCESSOR_IDENTIFIER",System.getenv("PROCESSOR_IDENTIFIER"));
+        map.put("COMPUTERNAME",System.getenv("PROCESSOR_IDENTIFIER"));
+        return new ResponseEntity(map, HttpStatus.OK);
+    }
+}
